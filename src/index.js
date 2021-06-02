d11b995a27e6758e240296bfbe5a9125a7f6c0ec
liuxichen <liuxichen@360.cn>
2021-06-02 15:39:13 +0800

    通过 resource 获取当前资源的实际类型来判断是否需要添加 loader

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 class MyPlugin {
   apply(compiler) {
-    compiler.hooks.thisCompilation.tap('MyPlugin', (compilation) => {
+    compiler.hooks.compilation.tap('MyPlugin', (compilation) => {
       // console.log('thisCompilation 初始化 compilation 时调用，在触发 compilation 事件之前调用')
       compilation.hooks.buildModule.tap('SourceMapDevToolModuleOptionsPlugin', module => {
-        if (/\.((j|t)sx?)$/.test(module.rawRequest)) {
-          if (module.loaders) {
-            module.loaders.push({
-              loader: 'element-rendering-webpack-loader'
-            })
+        if (module.resource) {
+          if (/(\.((j|t)sx?)$)/.test(module.resource) && 
+          !/node_modules/.test(module.resource)) {
+            if (module.loaders) {
+              module.loaders.push({
+                loader: 'element-rendering-webpack-loader'
+              })
+            }
           }
         }
-      })
-    })
+      })      
+    })  
   }
 }
 
